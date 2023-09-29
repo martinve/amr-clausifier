00850fb3281c1e313ca98e1d9dba34bd79d2d592
Martin Verrev <martin.verrev@ttu.ee>
2023-09-29 18:44:58 +0300

    Revert "Configurable models for AMR parsing"
    This reverts commit 430d2fa63f601c3239b7811b53881dd0f8615949.

--- .history/deleteme_20230901191658.py
+++
@@ -1,3 +0,0 @@
-"""
-Create and initialize bottle app with basic routing and sqlite support via sqlalchemy plugin and jinja2 for templates.
-"""
--- settings.py
+++ settings.py
@@ -4,8 +4,4 @@ protocol = "http"
 port = 9002
 host = "127.0.0.1"
 
-amr_server_port = 9003
-amr_server_host = "localhost"
-amr_parse_model = "parse_t5"
-
 debug_clauses = False
